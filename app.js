cb7377ba497b381460d23f1177ab7f59ba132dec
mayamaryam <125826954+mayamaryam@users.noreply.github.com>
2023-06-09 23:27:13 +0500

    Add files via upload
    OK

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
+
 // task1
-var age = 28;
-alert("I am " + age + "years old");
-// task2
-alert("you have visited thiss website 14 times");
-// task3
-var birthyear = 1992;
-document.write(" My birth year is " + birthyear);
-//   task4
-var VisitorName = "maryam";
-var ProductTitle = "maryam Clothing store";
-var Quantity = "5 T-shirt's ";
-document.write(VisitorName + " ordered " + Quantity + " on " + ProductTitle);
+var name, age, lastName;
+//   task2a
+var myName;
+var numberOne;
+var upperCase;
+var _MyName;
+var My_name;
+var $startWithDollar;
+//   task2b
+//   var 249;
+//   var hey ;
+//   var n 02;
+//   var legal-varibl:
+//   var legal variabl;
+// task 3
+var a = document.getElementById("heading");
+var b =
+  "Variables names can only contain , alphabet, $ ,uppercase and _. for example:$my_1stVariable";
+var c =
+  "Variables must begin with a letter, $ or _ . For example : $name,_name or name";
+var d = "Variables names are case sensitivee";
+var e = "Variables names should not be JS keyword";
+document.write(b + "<br>" + c + "<br>" + d + "<br>" + e);
+
+
