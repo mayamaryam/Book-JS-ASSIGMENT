9a22afcc692a8ebf3eddd77dc3e1a531631e47b5
mayamaryam <125826954+mayamaryam@users.noreply.github.com>
2023-06-09 23:24:08 +0500

    Add files via upload
    OK

--- app.js
+++ app.js
@@ -1,28 +1,10 @@
-function submit(){
-    var name= document.getElementById('name')
-    var email= document.getElementById('email')
-    alert("your name is "+name.value)
-    alert("your email is "+email.value)
-}
-
-
-function seemore(){
-    var seemore=document.getElementById('seemore')
-    var btn=document.getElementById('btn')
-    if(btn.innerHTML==="see more"){
-        seemore="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi iure eveniet et odit quae ipsa architecto adipisci repellendus vitae vero. Illum animi magni maxime deserunt eius earum asperiores autem similique!Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi iure eveniet et odit quae ipsa architecto adipisci repellendus vitae vero. Illum animi magni maxime deserunt eius earum asperiores autem similique!"
-        console.log(seemore)
-        document.getElementById('seemore').innerHTML=seemore
-        btn.innerHTML="see less"
-        btn.style.color="red"
-    }else{
-        seemore="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi iure eveniet et odit quae ipsa architecto adipisci repellendus vitae vero. Illum animi magni maxime deserunt eius earum asperiores autem similique!"
-        console.log(seemore)
-        document.getElementById('seemore').innerHTML=seemore
-        btn.innerHTML="see more"
-        btn.style.color="aqua"
-
-        
-    }
-
-}
+// task1
+alert("Hello there! Welcome to our website.");
+// task2
+alert("JavaScript Alert\nError! Please enter a valid password.");
+// task3
+alert("Welcome to JS Land \n Happy Coding!");
+// task4
+alert("JavaScript Alert \n Welcome to JS Land..");
+alert("JavaScript Alert \n Happy Coding!");
+alert("Prevent this page from creating additional dialogs.");
